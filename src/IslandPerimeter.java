0a90780cd311ae5e32d376ecd42a973f5f0083ba
Bleibtreu <742672464@qq.com>
2020-10-30 15:17:20 +0800

    小岛周长问题

---
+++ src/IslandPerimeter.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+/**
+ * 给定一个包含 0 和 1 的二维网格地图，其中 1 表示陆地, 0 表示水域。
+ * 网格中的格子水平和垂直方向相连（对角线方向不相连）。
+ * 整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+ * 岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。
+ * 格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。
+ * 计算这个岛屿的周长。
+ * <p>
+ * 示例 :
+ * 输入:
+ * [[0,1,0,0],
+ * [1,1,1,0],
+ * [0,1,0,0],
+ * [1,1,0,0]]
+ * 输出: 16
+ *
+ * @author bleibtreu
+ * @date 2020/10/30 14:10
+ */
+public class IslandPerimeter {
+
+    public static void main(String[] args) {
+        IslandPerimeter islandPerimeter = new IslandPerimeter();
+        int[][] grid = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+        System.out.println(islandPerimeter.islandPerimeter(grid));
+    }
+
+    public int islandPerimeter(int[][] grid) {
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == 1) {
+                    // 题目限制只有一个岛屿，计算一个即可
+                    return dfs(grid, r, c);
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * DFS算法
+     * @param grid
+     * @param r
+     * @param c
+     * @return
+     */
+    int dfs(int[][] grid, int r, int c) {
+        // 排除可能数组越界位置
+        if (!(0 <= r && r < grid.length && 0 <= c && c < grid[0].length)) {
+            return 1;
+        }
+        if (grid[r][c] == 0) {
+            return 1;
+        }
+        if (grid[r][c] != 1) {
+            return 0;
+        }
+
+        // 标记以遍历位置
+        grid[r][c] = 2;
+
+        // 进行递归
+        return dfs(grid, r - 1, c)
+                + dfs(grid, r + 1, c)
+                + dfs(grid, r, c - 1)
+                + dfs(grid, r, c + 1);
+    }
+}
